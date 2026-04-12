ba75b937b2f3939e7b45cbff5981533a02bda24e
Parth-FlutterDeveloper <patelparthm070@gmail.com>
2026-04-12 11:49:44 +0530

    User Side OTP Code Change

--- app/Http/Controllers/Api/User/Auth/AuthController.php
+++ app/Http/Controllers/Api/User/Auth/AuthController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 use Illuminate\Support\Facades\Mail;
 use Carbon\Carbon;
